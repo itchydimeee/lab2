d6a475e35526ee956d9a57857241867e4388eb58
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2023-11-09 19:38:52 +0800

    real
    dsd

--- app.ts
+++ app.ts
@@ -1,25 +1,122 @@
-import express from 'express';
-import crypto from ' crypto';
-import pool from './db';
+import http, { type IncomingMessage, type ServerResponse } from 'http'
+import fs from 'fs/promises'
+import { URLSearchParams } from 'url'
+import crypto from 'crypto'
+import {
+  saveLoanApplication,
+  type LoanApplication,
+  getLoanApplicationByToken
+} from './db'
 
-const router = express.Router();
+function createToken (): string {
+  return crypto.randomBytes(32).toString('base64url')
+}
 
-router.post('/apply-loan-success'), async (req, res) => {
-    const {/* from fields */ } = req.body;
+async function processForm (request: IncomingMessage): Promise<URLSearchParams> {
+  return await new Promise((resolve, reject) => {
+    let body = ''
+    request.on('data', chunk => {
+      body += chunk.toString()
+    })
+    request.on('end', () => {
+      const formData = new URLSearchParams(body)
+      resolve(formData)
+    })
+    request.on('error', error => {
+      reject(error)
+    })
+  })
+}
 
-    const uniqueToken = crypto.RandomBytes(64).toString('base64url');
+async function handleRequest (
+  request: IncomingMessage,
+  response: ServerResponse
+): Promise<void> {
+  const url = request.url
+  const method = request.method
 
+  if (url === '/apply-loan') {
+    const contents = await fs.readFile('./applyloan.html', 'utf8')
+    response.writeHead(200, { 'Content-Type': 'text/html' }).end(contents)
+  } else if (url === '/apply-loan-success' && method === 'POST') {
     try {
-        const client = await pool.connect();
-        await client.query('INSERT INTO loans(/* columns */) VALUES(/* values */)', [/* values */]);
-        client.release()
-        res.redirect(`/loan-status/${uniqueToken}`);
+      const formData = await processForm(request)
 
-    }
-    catch (error) {
-        console.error('Error:', error);
-        res.redirect('/error');
-    }
-});
+      const applicationData: LoanApplication = {
+        name: formData.get('username') ?? '',
+        email: formData.get('email') ?? '',
+        phone_number: parseInt(formData.get('phoneNum') ?? '0', 10),
+        loan_amount: parseInt(formData.get('loanAmount') ?? '0', 10),
+        loan_reason: formData.get('reason') ?? '',
+        loan_status: 'APPLIED',
+        unique_token: createToken()
+      }
 
-export default router
+      await saveLoanApplication(applicationData)
+      response
+        .writeHead(200, { 'Content-Type': 'text/plain' })
+        .end('Loan application successful!')
+    } catch (error) {
+      console.error('Error:', error)
+      response
+        .writeHead(500, { 'Content-Type': 'text/html' })
+        .end('Error')
+    }
+  } else {
+    if (url != null && url.includes('/')) {
+      const urlParts = url.split('/')
+      const uniqueToken = urlParts[urlParts.length - 1]
+      try {
+        const loanApplication = await getLoanApplicationByToken(uniqueToken)
+
+        if (loanApplication != null) {
+          const html = `
+              <!DOCTYPE html>
+              <html lang="en">
+    
+              <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Loan Application Details</title>
+              </head>
+    
+              <body>
+                <h1>Loan Application Details</h1>
+                <p>Name: ${loanApplication.name}</p>
+                <p>Email: ${loanApplication.email}</p>
+                <p>Phone Number: ${loanApplication.phone_number}</p>
+                <p>Loan Amount: ${loanApplication.loan_amount}</p>
+                <p>Loan Reason: ${loanApplication.loan_reason}</p>
+                <p>Loan Status: ${loanApplication.loan_status}</p>
+                <p>Unique Token: ${loanApplication.unique_token}</p>
+              </body>
+    
+              </html>
+            `
+
+          response.writeHead(200, { 'Content-Type': 'text/html' }).end(html)
+        } else {
+          response
+            .writeHead(404, { 'Content-Type': 'text/html' })
+            .end('You sent me: ' + url)
+        }
+      } catch (error) {
+        console.error('Error:', error)
+        response
+          .writeHead(500, { 'Content-Type': 'text/html' })
+          .end('Error')
+      }
+    } else {
+      response
+        .writeHead(400, { 'Content-Type': 'text/html' })
+        .end('Error')
+    }
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+const server = http.createServer(handleRequest)
+
+server.listen(3000, () => {
+  console.log('Server started at http://localhost:3000')
+})
--- db.ts
+++ db.ts
@@ -1,11 +1,77 @@
-import { Pool } from 'pg';
+import { Pool } from 'pg'
 
 const pool = new Pool({
-  user: 'your_username',
+  user: 'postgres',
   host: 'localhost',
-  database: 'your_database_name',
-  password: 'your_password',
+  password: 'bambam073004',
+  database: 'postgres',
   port: 5432
 })
 
-export default pool;
+// Interface for the loan application data
+interface LoanApplication {
+  name: string
+  email: string
+  phone_number: number
+  loan_amount: number
+  loan_reason: string
+  loan_status: string
+  unique_token: string
+  approval_rejection_date?: Date
+  cash_release_date?: Date
+  repayment_date?: Date
+}
+
+// Function to save the loan application data to the database
+export async function saveLoanApplication (
+  applicationData: LoanApplication
+): Promise<void> {
+  try {
+    // SQL query to insert the application data into the loans table
+    const query = `
+            INSERT INTO loans (name, email, phone_number, loan_amount, loan_reason, loan_status, unique_token) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7) 
+            RETURNING *;
+        `
+
+    // Execute the SQL query with applicationData values
+    const values = await pool.query(query, [
+      applicationData.name,
+      applicationData.email,
+      applicationData.phone_number,
+      applicationData.loan_amount,
+      applicationData.loan_reason,
+      applicationData.loan_status,
+      applicationData.unique_token
+    ])
+
+    // Log the inserted row (if needed)
+    console.log('Inserted row:', values)
+  } catch (error) {
+    // Handle errors (log them and throw if necessary)
+    console.error('Error saving loan application:', error)
+    throw error
+  }
+}
+
+export async function getLoanApplicationByToken (
+  token: string
+): Promise<LoanApplication | null> {
+  try {
+    const client = await pool.connect()
+    const query = 'SELECT * FROM loans WHERE unique_token = $1;'
+    const { rows } = await client.query(query, [token])
+
+    client.release()
+
+    return rows.length > 0 ? rows[0] : null
+  } catch (error) {
+    console.error('Error fetching loan application:', error)
+    throw error
+  }
+}
+
+// Export the function for external use
+// export { saveLoanApplication }
+export type { LoanApplication }
+export { pool }
